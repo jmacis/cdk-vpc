3c797f21aaeba2b16462b72797e2f00c54026f17
John Macis <johnmacis@gmail.com>
2020-08-06 10:13:18 -0400

    update files

--- test/cdk-vpc.test.ts
+++ test/cdk-vpc.test.ts
@@ -3,11 +3,11 @@ import * as cdk from '@aws-cdk/core';
 import * as CdkVpc from '../lib/cdk-vpc-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkVpc.CdkVpcStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkVpc.CdkVpcStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(matchTemplate({
+    "Resources": {}
+  }, MatchStyle.EXACT))
 });
